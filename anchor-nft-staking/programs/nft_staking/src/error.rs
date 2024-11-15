0fead6043ff4c9b02fefd724253c6bc8c146820d
Ayush <ayushagarwal.dev@gmail.com>
2024-11-16 00:06:31 +0530

    add submodule

--- anchor-nft-staking/programs/nft_staking/src/error.rs
+++
@@ -1,9 +0,0 @@
-use anchor_lang::error_code;
-
-#[error_code]
-pub enum StakeError   {
-    #[msg("Max Stake Reached")]
-    MaxStakeReached,
-    #[msg("Freeze Period Not Over")]
-    FreezePeriodNotOver
-}
--- anchor-nft-staking/programs/nft_staking/src/state/user_account.rs
+++
@@ -1,9 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[account]
-#[derive(InitSpace)]
-pub struct UserAccount {
-    pub points: u32,
-    pub amounts_staked:u8,
-    pub bump:u8
-}
